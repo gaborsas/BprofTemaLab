833550795bfbbad32fbf8e7c34a586c8a0d19bfc
gaborsas <cvekedli09@gmail.com>
2020-03-08 20:49:53 +0100

    Új termék és beszállító  szerkesztő oldal

--- BProfTemaLab/Controllers/HomeController.cs
+++ BProfTemaLab/Controllers/HomeController.cs
@@ -33,6 +33,16 @@ namespace BProfTemaLab.Controllers
             return View();
         }
 
+        public IActionResult UjBeszallito()
+        {
+            return View();
+        }
+
+        public IActionResult UjTermek()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
