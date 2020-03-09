17137084c2c05141b178a6c15df5c5436b80a1ca
gaborsas <cvekedli09@gmail.com>
2020-03-09 14:34:15 +0100

    Admin oldal létrehozása, felhasználó táblázat, Beszállító oldalnál a termékekhez a hozáadás és keresés megjelenítése

--- BProfTemaLab/Controllers/HomeController.cs
+++ BProfTemaLab/Controllers/HomeController.cs
@@ -43,6 +43,11 @@ namespace BProfTemaLab.Controllers
             return View();
         }
 
+        public IActionResult Admin()
+        {
+            return View();
+        }
+
         public IActionResult Stand()
         {
             return View();
