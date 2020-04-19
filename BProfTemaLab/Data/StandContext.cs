c6898e393dd6699b57b4b0d549f56554dcb91b73
Daniel-Randek <radani94@gmail.com>
2020-04-19 04:27:42 +0200

    StandContext

--- BProfTemaLab/Data/StandContext.cs
+++ BProfTemaLab/Data/StandContext.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using BProfTemaLab.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
 
 namespace BProfTemaLab.Data
 {
-    public class StandContext
+    public class StandContext : DbContext
     {
+        public StandContext(DbContextOptions options) : base(options) { }
+        public DbSet<Termek> Termekek { get; set; }
+
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Termek>().HasData(
+
+                new Termek
+                {
+                    ID = 164,
+                    Nev = @"Assassin's Creed: Reneszánsz",
+                    BeszallitoID = 1,
+                    EgysegAr = 20,
+                    DarabPerRekesz = 480
+                }
+            );
+        }*/
     }
 }
--- BProfTemaLab/Startup.cs
+++ BProfTemaLab/Startup.cs
@@ -32,6 +32,8 @@ namespace BProfTemaLab
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDbContext<StandContext>( o => o.UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
