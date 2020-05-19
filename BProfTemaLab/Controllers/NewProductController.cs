06c3da6f20f76f24b6de1d41650703d35d308dec
gaborsas <cvekedli09@gmail.com>
2020-05-19 23:54:38 +0200

    Beszállító oldalon a nyilak, delete gomb elhelyezése, New product oldalon a select megcsinálása

--- BProfTemaLab/Controllers/NewProductController.cs
+++ BProfTemaLab/Controllers/NewProductController.cs
@@ -6,28 +6,40 @@ using BProfTemaLab.Dal.Dtos;
 using BProfTemaLab.Dal.Entities;
 using BProfTemaLab.Dal.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BProfTemaLab.Controllers
 {
     public class NewProductController : Controller
     {
         public ProductService ProductService;
-        public NewProductController(ProductService productService)
+        public SupplierService SupplierService;
+        public NewProductController(ProductService productService, SupplierService supplierService)
         {
             ProductService = productService;
+            SupplierService = supplierService;
         }
         public IActionResult NewProduct()
         {
+            List<SelectListItem> suppliers = new List<SelectListItem>();
+            suppliers = SupplierService.DbContext.Supplier
+                .Select(s => new SelectListItem {
+                    Text = s.Name,
+                    Value = s.Id.ToString()
+                }).ToList();
+
+ 
+            ViewBag.MySkills = suppliers;  
             return View();
         }
 
         [HttpPost]
-        public ActionResult AddProduct(string name, int unitPrice, int quantity) 
+        public ActionResult AddProduct(int supplierId, string name, int unitPrice, int quantity) 
         {
             var product = new Product
             {
                 Name = name,
-                SupplierId = 1,
+                SupplierId = supplierId,
                 UnitPrice = unitPrice,
                 Quantity = quantity
             };
