041b684de1ed7882c2c2ff82b58469b1cd3b37c3
Lévai Levente <levai.levente01@gmail.com>
2020-05-19 20:06:20 +0200

    Identity

---
+++ BProfTemaLab.Dal/SeedService/RoleSeedService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BProfTemaLab.Dal.SeedInterfaces;
+using BProfTemaLab.Dal.Users;
+using Microsoft.AspNetCore.Identity;
+
+namespace BProfTemaLab.Dal.SeedService
+{
+    public class RoleSeedService :IRoleSeedService
+    {
+        private readonly RoleManager<IdentityRole<int>> _roleManager;
+        public RoleSeedService(RoleManager<IdentityRole<int>> roleManager) =>
+            _roleManager = roleManager;
+        public async Task SeedRoleAsync()
+        {
+            if (!await _roleManager.RoleExistsAsync(Roles.Administrators))
+                await _roleManager.CreateAsync(new IdentityRole<int> { Name = Roles.Administrators });
+
+            if (!await _roleManager.RoleExistsAsync(Roles.Suppliers))
+                await _roleManager.CreateAsync(new IdentityRole<int> { Name = Roles.Suppliers });
+
+            if (!await _roleManager.RoleExistsAsync(Roles.Workers))
+                await _roleManager.CreateAsync(new IdentityRole<int> { Name = Roles.Workers });
+        }
+    }
+}
---
+++ BProfTemaLab.Dal/SeedService/UserSeedService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BProfTemaLab.Dal.Entities;
+using BProfTemaLab.Dal.SeedInterfaces;
+using BProfTemaLab.Dal.Users;
+using Microsoft.AspNetCore.Identity;
+
+namespace BProfTemaLab.Dal.SeedService
+{
+    public class UserSeedService: IUserSeedService
+    {
+        private readonly UserManager<User> _userManager;
+        public UserSeedService(UserManager<User> userManager) =>
+            _userManager = userManager;
+        public async Task SeedUserAsync()
+        {
+            if (!(await _userManager.GetUsersInRoleAsync(Roles.Administrators)).Any())
+            {
+                var user = new User
+                {
+                    Email = "admin@bprofstand.hu",
+                    Name = "TesztAdmin",
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    UserName = "admin"
+                };
+                var createResult = await _userManager.CreateAsync(user, "Asdasd123!");
+                var addToRoleResult = await _userManager.AddToRoleAsync(user, Roles.Administrators);
+                if (!createResult.Succeeded || !addToRoleResult.Succeeded)
+                    throw new ApplicationException($"Administrator could not be created: " +
+                                                   $"{string.Join(", ", createResult.Errors.Concat(addToRoleResult.Errors).Select(e => e.Description))}");
+            }
+
+            if (!(await _userManager.GetUsersInRoleAsync(Roles.Suppliers)).Any())
+            {
+                var user = new User
+                {
+                    Email = "supplier@bprofstand.hu",
+                    Name = "Teszt_Supplier",
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    UserName = "supplier"
+                };
+                var createResult = await _userManager.CreateAsync(user, "Asdasd123!");
+                var addToRoleResult = await _userManager.AddToRoleAsync(user, Roles.Suppliers);
+                if (!createResult.Succeeded || !addToRoleResult.Succeeded)
+                    throw new ApplicationException($"Supplier could not be created: " +
+                                                   $"{string.Join(", ", createResult.Errors.Concat(addToRoleResult.Errors).Select(e => e.Description))}");
+            }
+
+            if (!(await _userManager.GetUsersInRoleAsync(Roles.Workers)).Any())
+            {
+                var user = new User
+                {
+                    Email = "worker@bprofstand.hu",
+                    Name = "Teszt_Worker",
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    UserName = "worker"
+                };
+                var createResult = await _userManager.CreateAsync(user, "Asdasd123!");
+                var addToRoleResult = await _userManager.AddToRoleAsync(user, Roles.Workers);
+                if (!createResult.Succeeded || !addToRoleResult.Succeeded)
+                    throw new ApplicationException($"Worker could not be created: " +
+                                                   $"{string.Join(", ", createResult.Errors.Concat(addToRoleResult.Errors).Select(e => e.Description))}");
+            }
+        }
+
+    }
+}
