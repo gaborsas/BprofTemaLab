3a36de043cd7ee1d2873a8f9e5dba397b6921319
Daniel-Randek <radani94@gmail.com>
2020-05-20 15:28:07 +0200

    Standentityconfiguration

---
+++ BProfTemaLab.Dal/EntityConfigurations/StandEntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using BProfTemaLab.Dal.Entities;
+using BProfTemaLab.Dal.SeedService;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BProfTemaLab.Dal.EntityConfigurations
+{
+    class StandEntityConfiguration : IEntityTypeConfiguration<Stand>
+    {
+        private readonly ISeedService _seedService;
+        public StandEntityConfiguration(ISeedService seedService)
+        => _seedService = seedService;
+        public void Configure(EntityTypeBuilder<Stand> builder)
+        => builder.HasData(_seedService.Products);
+    }
+}
